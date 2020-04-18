ec85930631e412a3da7001d0e77d98f4ed1fafc5
Anthony Roux <m.roux.a@gmail.com>
2020-04-18 16:42:40 +0200

    Fix issue with wrong FlightMinusoffer object and add example for post and get by URL

--- amadeus-android/src/test/java/com/amadeus/android/service/AmadeusTest.kt
+++ amadeus-android/src/test/java/com/amadeus/android/service/AmadeusTest.kt
@@ -1,11 +1,12 @@
 package com.amadeus.android.service
 
 import com.amadeus.android.Amadeus
-import com.amadeus.android.BuildConfig
 import com.amadeus.android.base.ApiResult
 import com.amadeus.android.base.ApiResult.Success
 import com.amadeus.android.base.succeeded
+import com.amadeus.android.BuildConfig
 import com.amadeus.android.domain.air.models.AirTraffic
+import com.amadeus.android.domain.air.models.FlightMinusoffer
 import com.amadeus.android.domain.air.models.Location
 import com.amadeus.android.domain.air.tools.TypesAdapterFactory
 import com.amadeus.android.domain.air.tools.XNullableAdapterFactory
@@ -19,6 +20,7 @@ import org.junit.Test
 import org.junit.runners.MethodSorters
 import org.threeten.bp.Clock
 import org.threeten.bp.LocalDate
+
 
 @Suppress("BlockingMethodInNonBlockingContext")
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
@@ -289,5 +291,39 @@ class AmadeusTest {
     @Test
     fun `Seat map for offer id`() = runBlocking {
         assert(amadeus.shopping.seatMaps.get("eJzTd9f3NjIJdzUGAAp%2fAiY=")?.succeeded ?: false)
+    }
+
+    @Test
+    fun `test get`() = runBlocking {
+        val get = amadeus.get("/v2/shopping/hotel-offers?cityCode=PAR&adults=1&radius=5&radiusUnit=KM&paymentPolicy=NONE&includeClosed=false&bestRateOnly=true&view=FULL&sort=PRICE")
+        print(get)
+    }
+
+    @Test
+    fun `test post`() = runBlocking {
+        val flightOffer = amadeus.shopping.flightOffersSearch.get(
+            "MAD",
+            "MUC",
+            LocalDate.now(Clock.systemUTC()).plusMonths(1),
+            1
+        )
+
+        val type = Types.newParameterizedType(
+            List::class.java,
+            FlightMinusoffer::class.java
+        )
+        val resultType = Types.newParameterizedTypeWithOwner(
+            ApiResult::class.java,
+            Success::class.java,
+            type
+        )
+
+        val jsonAdapter = moshi.adapter<ApiResult<List<FlightMinusoffer>>>(resultType)
+        val jsonFlightOffers: String = jsonAdapter.toJson(flightOffer)
+        print("----")
+        print(jsonFlightOffers)
+        print("----")
+        val post = amadeus.post("/v2/shopping/flight-offers/prediction", jsonFlightOffers)
+        print(post)
     }
 }
