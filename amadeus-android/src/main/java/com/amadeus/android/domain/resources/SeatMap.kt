585411bfc5ad4a3802a2b1e50fe3e0c3149f2447
Anna Tsolakou <anna.tsolakou@amadeus.com>
2021-02-18 13:10:41 +0100

    add amenities and seat media to seatmap resource

--- amadeus-android/src/main/java/com/amadeus/android/domain/resources/SeatMap.kt
+++ amadeus-android/src/main/java/com/amadeus/android/domain/resources/SeatMap.kt
@@ -19,7 +19,9 @@ data class SeatMap internal constructor(
     val number: String? = null,
     val aircraft: Aircraft? = null,
     val departure: Departure? = null,
-    val decks: List<Deck>? = null
+    val decks: List<Deck>? = null,
+    val aircraftCabinAmenities: AircraftCabinAmenities? = null,
+    val availableSeatsCounters: List<AvailableSeatsCounter>? = null
 ) : Parcelable {
 
     @Parcelize
@@ -59,5 +61,84 @@ data class SeatMap internal constructor(
         val endWingsRow: Int = 0,
         val startWingsX: Int = 0,
         val endWingsX: Int = 0
+    ) : Parcelable
+
+    @Parcelize
+    @JsonClass(generateAdapter = true)
+    data class AircraftCabinAmenities internal constructor(
+        val power: AmenityPower? = null,
+        val wifi: AmenityWifi? = null,
+        val beverage: AmenityBeverage? = null,
+        val food: AmenityFood? = null,
+        val entertainment: List<AmenityEntertainment>? = null,
+        val seat: AmenitySeat? = null
+    ) : Parcelable
+
+    @Parcelize
+    @JsonClass(generateAdapter = true)
+    data class AmenityPower internal constructor(
+        val powerType: String? = null,
+        val isChargeable: Boolean? = null
+    ) : Parcelable
+
+    @Parcelize
+    @JsonClass(generateAdapter = true)
+    data class AmenityWifi internal constructor(
+        val wifiCoverage: String? = null,
+        val isChargeable: Boolean? = null
+    ) : Parcelable
+
+    @Parcelize
+    @JsonClass(generateAdapter = true)
+    data class AmenityBeverage internal constructor(
+        val beverageType: String? = null,
+        val isChargeable: Boolean? = null
+    ) : Parcelable
+
+    @Parcelize
+    @JsonClass(generateAdapter = true)
+    data class AmenityFood internal constructor(
+        val foodType: String? = null,
+        val isChargeable: Boolean? = null
+    ) : Parcelable
+
+    @Parcelize
+    @JsonClass(generateAdapter = true)
+    data class AmenityEntertainment internal constructor(
+        val entertainmentType: String? = null,
+        val isChargeable: Boolean? = null
+    ) : Parcelable
+
+    @Parcelize
+    @JsonClass(generateAdapter = true)
+    data class AmenitySeat internal constructor(
+        val legSpace: Int? = null,
+        val spaceUnit: String? = null,
+        val tilt: String? = null,
+        val amenityType: String? = null,
+        val medias: List<Media>? = null
+    ) : Parcelable
+
+    @Parcelize
+    @JsonClass(generateAdapter = true)
+    data class AvailableSeatsCounter internal constructor(
+        val value: Int? = null,
+        val travelerId: String? = null
+    ) : Parcelable
+
+    @Parcelize
+    @JsonClass(generateAdapter = true)
+    data class Media internal constructor(
+        val title: String? = null,
+        val href: String? = null,
+        val mediaType: String? = null,
+        val description: QualifiedFreeText? = null
+    ) : Parcelable
+
+    @Parcelize
+    @JsonClass(generateAdapter = true)
+    data class QualifiedFreeText internal constructor(
+        val text: String? = null,
+        val lang: String? = null
     ) : Parcelable
 }
