aa658a0c6e0310b312dbe08c905d8f796e9cadc8
Nicolas Pietri <nico.pietri@gmail.com>
2021-03-06 18:47:18 +0100

    configure mockk values for error adapter and map adapter

--- amadeus-android/src/test/java/com/amadeus/android/unit/api/BaseTest.kt
+++ amadeus-android/src/test/java/com/amadeus/android/unit/api/BaseTest.kt
@@ -1,8 +1,10 @@
 package com.amadeus.android.unit.api
 
+import com.amadeus.android.Amadeus
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineDispatcher
@@ -25,6 +27,12 @@ abstract class BaseTest {
         fun setupApi() {
             MockKAnnotations.init(this)
             Dispatchers.setMain(dispatcher)
+
+            Amadeus.mapAdapter = mockk(relaxed = true)
+            every { Amadeus.mapAdapter.fromJson("") } returns mapOf()
+
+            Amadeus.errorAdapter = mockk(relaxed = true)
+            every { Amadeus.errorAdapter.fromJson("") } returns mockk()
         }
     }
 
